c87ed65e50bb882290ce7e9294967d56e17ff755
Gerhard Lipp <gelipp@googlemail.com>
2012-05-04 22:38:37 +0200

    results of lab

--- wappcam.lua
+++ wappcam.lua
@@ -1,7 +1,55 @@
-#!/usr/bin/env orbit
-local orbit = require"orbit"
+#!/usr/bin/env lua
+cv=require('luacv')
+require('orbit')
 local wappcam = orbit.new('wappcam')
 
-wappcam:dispatch_static('.+')
+--[[local getindex = 
+  function()
+    local fileindex = io.open('static/index.html')
+    local index = fileindex:read('*a')
+  return index
+end]]--
+
+local loadimage = 
+  function(path)
+    local image = cv.LoadImage(path)
+    return image
+  end	
+
+local getimage =  
+  function(web)
+    local imageindex = web.GET.file
+    local gainconst = web.GET.gain
+    local shutterconst = web.GET.shutter
+
+    local image = loadimage('static/'..imageindex..'.jpg')
+    local noise = loadimage('static/noise.jpg')
+
+    local shutter = 50+(shutterconst-100)
+    local gain  = 1-(gainconst/100)
+
+    local noise_resize = cv.CloneImage(image)
+    local dst = cv.CloneImage(image)
+    cv.Resize(noise,noise_resize)
+    cv.AddWeighted(image,1,noise_resize,gain,shutter,dst)
+    web.headers['Content-Type'] = 'image/jpeg'
+    web.headers['Content-Length'] = #image
+  return image 
+end
+
+--[[local getcam =
+	function()
+	local filecam = io.open('static/lkdemo.lua')
+	local cam= filecam:read('*a')
+    --web.headers['Content-Type'] = 'image/jpeg'
+    --web.headers['Content-Length'] = #image
+  return cam 
+end]]--
+
+
+wappcam:dispatch_get(getimage,'/image')
+wappcam:dispatch_static('/static/.+')
+--wappcam:dispatch_get(getindex,'/')
+--wappcam:dispatch_get(getcam,'/cam')
 
 return wappcam
