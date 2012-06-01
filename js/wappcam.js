e0ea5b8bcbfd0d1c56b0c47542f1e5d65d5f6b33
Gerhard Lipp <gerhard_lipp@gmx.de>
2012-06-01 11:42:33 +0200

    showing fake images, integrating server and client

--- js/wappcam.js
+++ js/wappcam.js
@@ -2,19 +2,27 @@ $(function(){
     // pseudo main 
 
 
-    var gain = 50;
-    var shutter = 50;
-    var brightness = 50;
-    var contrast = 50;
-    var saturation = 50;
+var gain = 0;
+var shutter = 50;
+var brightness = 50;
+var contrast = 50;
+var saturation = 50;
+var i = 0;
 
-    $('#cameraImage').attr("localhost:8080/image?gain="+Math.floor(gain)+"&shutter="+Math.floor(shutter)+"&brightness="+Math.floor(brightness)+"&contrast="+Math.floor(contrast)+"&saturation="+Math.floor(saturation))
+var update_image = function() {
+  var img_url = "/image?gain="+Math.floor(gain)+"&shutter="+Math.floor(shutter)+"&brightness="+Math.floor(brightness)+
+	      "&contrast="+Math.floor(contrast)+"&saturation="+Math.floor(saturation)+"&i="+i+1;  
+  $('#cameraImage').attr("src",img_url);	
+}
+   
+
 
     $("#gainslider").noUiSlider("init",{
         dontActivate: "lower", startMax: gain,
         change: function() {
             gain = $(this).noUiSlider('getValue')[0];
             console.log(gain);
+	    update_image();
         }
     });
     $("#shutterslider").noUiSlider("init",{
@@ -22,6 +30,7 @@ $(function(){
         change: function() {
             shutter = $(this).noUiSlider('getValue')[0];
             console.log(shutter);
+	    update_image();
         }
     });
     $("#brightnessslider").noUiSlider("init",{
@@ -29,6 +38,7 @@ $(function(){
         change: function() {
             brightness = $(this).noUiSlider('getValue')[0];
             console.log(brightness);
+	    update_image();
         }
     });
     $("#contrastslider").noUiSlider("init",{
@@ -36,6 +46,7 @@ $(function(){
         change: function() {
             contrast = $(this).noUiSlider('getValue')[0];
             console.log(contrast);
+	    update_image();
         }
     });
     $("#saturationslider").noUiSlider("init",{
@@ -43,57 +54,71 @@ $(function(){
         change: function() {
             saturation = $(this).noUiSlider('getValue')[0];
             console.log(saturation);
+	    update_image();
         }
     });
 
-    var testpics = ["img/kirlian30.jpg","img/09678925.jpg","img/kamera.jpg","img/6730.jpg","img/kirlian15.jpg","img/kirlian03.jpg","img/100325_simpsons.jpg"];
-    var picsrc = new Array(7);
-    var i=0;
+ 
+var testpics = ["http://www.mpia.de/Public/Aktuelles/PR/2009/PR090205/PR090205_2ohr.jpg",
+"http://static.pagenstecher.de/uploads/4/40/408/408f/homer20simpson.jpg",
+"http://www.bilder.beofnf.de/wallpaper/simpsons.jpg",
+"http://www.testedich.de/quiz25/picture/pic_1216037428_3.jpg",
+"http://images3.wikia.nocookie.net/__cb20100626113247/reddeadredemption/images/1/11/Bart_Simpson.jpg",
+"http://images2.wikia.nocookie.net/__cb20081212024429/inciclopedia/images/3/32/Homero-diablo-satans-los-simpson.jpg",
+"http://img1.gbpicsonline.com/gb/193/009.jpg"];
+var picsrc = new Array(7);
+var i=0;
 
-    $("#single").click(function(){        
-        i=i+1;
-        var newpic=testpics[i%7];
-        picsrc.unshift(newpic);
-        picsrc.pop();
 
-        $("#cameraImage").attr("src",picsrc[0]);
-        $("#cameraImage1").attr("src",picsrc[1]);
-        $("#cameraImage2").attr("src",picsrc[2]);
-        $("#cameraImage3").attr("src",picsrc[3]);
-        $("#cameraImage4").attr("src",picsrc[4]);
-        $("#cameraImage5").attr("src",picsrc[5]);
-        $("#cameraImage6").attr("src",picsrc[6]);
 
-    });
 
-    var clicked = false;
-    var i=0;
+$("#single").click(function(){
 
-    var start = function(){
-        i = i+1 
-        var newpic=testpics[i%7];
-        picsrc.unshift(newpic);
-        picsrc.pop();
+   
+i=i+1;
+var newpic=testpics[i%7];
+picsrc.unshift(newpic);
+picsrc.pop();
 
-        $("#cameraImage").attr("src",picsrc[0]);
-        $("#cameraImage1").attr("src",picsrc[1]);
-        $("#cameraImage2").attr("src",picsrc[2]);
-        $("#cameraImage3").attr("src",picsrc[3]);
-        $("#cameraImage4").attr("src",picsrc[4]);
-        $("#cameraImage5").attr("src",picsrc[5]);
-        $("#cameraImage6").attr("src",picsrc[6]);
-    };
+$("#cameraImage").attr("src",picsrc[0]);
+$("#cameraImage1").attr("src",picsrc[1]);
+$("#cameraImage2").attr("src",picsrc[2]);
+$("#cameraImage3").attr("src",picsrc[3]);
+$("#cameraImage4").attr("src",picsrc[4]);
+$("#cameraImage5").attr("src",picsrc[5]);
+$("#cameraImage6").attr("src",picsrc[6]);
 
-    var t = 0;
+});
 
-    $('#sequence').click(function(){
-        clicked = !clicked;
-        if (clicked){
-            t = setInterval(start, 200);
-        } else {
-            clearInterval(t);
-        }
-    });   
+var clicked = false;
+var i=0;
+
+var start = function(){
+   i = i+1 
+  var newpic=testpics[i%7];
+  picsrc.unshift(newpic);
+  picsrc.pop();
+
+  $("#cameraImage").attr("src",picsrc[0]);
+  $("#cameraImage1").attr("src",picsrc[1]);
+  $("#cameraImage2").attr("src",picsrc[2]);
+  $("#cameraImage3").attr("src",picsrc[3]);
+  $("#cameraImage4").attr("src",picsrc[4]);
+  $("#cameraImage5").attr("src",picsrc[5]);
+  $("#cameraImage6").attr("src",picsrc[6]);
+
+};
+
+var t = 0;
+
+   $('#sequence').click(function(){
+     clicked = !clicked;
+     if (clicked){
+     t = setInterval(start, 200);
+     } else {
+     clearInterval(t);
+     }
+     });   
 
 });
 
--- wappcam.lua
+++ wappcam.lua
@@ -1,41 +1,87 @@
-#!/usr/bin/env lua
+#!/usr/bin/env lua							-- Einbindung von relevanten Kackdingern
 cv=require('luacv')
 require('orbit')
-local wappcam = orbit.new('wappcam')
 
---[[local getindex = 
-  function()
-    local fileindex = io.open('static/index.html')
-    local index = fileindex:read('*a')
-  return index
-end]]--
+local wappcam = orbit.new('wappcam')					-- neues Wappcam-Objekt
 
-local loadimage = 
+local gain = 0								-- Start Wert gain
+local shutter = 50							-- Start Wert shutter
+local brightness = 50							-- Start Wert brightness
+local contrast = 50							-- Start Wert contrast
+local saturation = 50							-- Start Wert saturation
+
+
+local loadimage = 							-- Funktion fürs Bild einladen
   function(path)
-    local image = cv.LoadImage(path)
+    local fileindex = io.open(path)
+    local image=fileindex:read("*a")
     return image
   end	
 
-local getimage =  
+local redirectindex = 							-- Funktion für die Index-Seite
   function(web)
-    local imageindex = web.GET.file
-    local gainconst = web.GET.gain
-    local shutterconst = web.GET.shutter
+    web:redirect('/index.html')
+    return 
+  end	
 
-    local image = loadimage('static/'..imageindex..'.jpg')
-    local noise = loadimage('static/noise.jpg')
+local make_fake_image = 						-- Fakebild kreieren
+  function(imagename, options)
+    print(options.gain)
+    print(options.brightness)
+    
+    local src = cv.LoadImage('static/'..(imagename or 'testa')..'.jpg')	-- fakebild laden
+    local noise = cv.LoadImage('static/noise.jpg')			-- fakerauschen laden
 
-    local shutter = 50+(shutterconst-100)
-    local gain  = 1-(gainconst/100)
+    local gamma = 2*(options.brightness-50)				-- shutter anpassung
+    local Alpha = 1-(options.gain/100)
+    local Beta  = options.gain/100					-- gain nomiert
+    local noise_resize = cv.CloneImage(src)				-- größe des rauschbildes angleichen
 
-    local noise_resize = cv.CloneImage(image)
-    local dst = cv.CloneImage(image)
-    cv.Resize(noise,noise_resize)
-    cv.AddWeighted(image,1,noise_resize,gain,shutter,dst)
-    web.headers['Content-Type'] = 'image/jpeg'
-    web.headers['Content-Length'] = #image
-  return image 
+    local dst = cv.CloneImage(src)					-- resultat anlegen
+    cv.Resize(noise,noise_resize)					-- noise in noise_resize einfügen
+    cv.AddWeighted(src,Alpha,noise_resize,Beta,gamma,dst)		-- Methode wird ausgeführt dst=(Image*Alpha+Noise_resize*beta)+gamma
+    cv.SaveImage('Fakebild.jpg', dst)					-- Bild speichern
+    local image = loadimage('Fakebild.jpg')
+    return image,'jpeg'
 end
+
+
+local query_image=make_fake_image  					-- SCHALTER
+
+
+local getimage =  
+  function(web)    
+    local imagename = web.GET.file
+print('hallo',imagename)
+    local options = 							-- Options Tabelle
+		    {gain=tonumber(web.GET.gain) or gain, shutter=tonumber(web.GET.shutter)or shutter, brightness=tonumber(web.GET.brightness)or brightness,
+		    contrast=tonumber(web.GET.contrast)or contrast, saturation=tonumber(web.GET.saturation)or saturation}
+
+    for i,v in pairs(options) do 					--  For Loop zur Drosselung der Eingabewerte
+      if v > 100 then
+	  options[i] = 100
+      elseif  v < 0 then
+	  options[i] = 0
+      end
+  
+    end
+    local image_data,type = query_image(imagename, options)
+    web.headers['Content-Type'] = 'image/'..type			-- Content Typ anpassen
+    web.headers['Content-Length'] = #image_data				-- content length anpassen
+print('ok')
+  return image_data 							-- Rückgabe Image an den server
+end
+
+
+wappcam:dispatch_get(getimage,'/image')					-- funktion getimage aufrufen bei .../image
+wappcam:dispatch_static('/index.html')
+wappcam:dispatch_get(redirectindex,'/')
+wappcam:dispatch_static('/css/.+')	
+wappcam:dispatch_static('/js/.+')
+wappcam:dispatch_static('/img/.+')					-- .../static/x ruft datei mit dem namen x aus dem ordner static auf
+
+return wappcam
+
 
 --[[local getcam =
 	function()
@@ -45,11 +91,3 @@ end
     --web.headers['Content-Length'] = #image
   return cam 
 end]]--
-
-
-wappcam:dispatch_get(getimage,'/image')
-wappcam:dispatch_static('/static/.+')
---wappcam:dispatch_get(getindex,'/')
---wappcam:dispatch_get(getcam,'/cam')
-
-return wappcam
