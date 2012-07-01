65e3cac6c71764f0f02c8946ac33daafe33dcb92
Gerhard Lipp <gelipp@googlemail.com>
2012-07-01 09:42:18 +0200

    doc

--- js/wappcam.js
+++ js/wappcam.js
@@ -1,7 +1,13 @@
+// -Initials all sliders,
+// -installs clickhandlers for single- and life-image button, 
+// -sets interval for live-image,
+// -manage image history
+
+
 $(function(){
     // pseudo main
 
-
+//sets the attribute for the first pic 
     var gain = 0;
     var shutter = 50;
     var brightness = 50;
@@ -20,8 +26,9 @@ $(function(){
                     "http://img1.gbpicsonline.com/gb/193/009.jpg"];
     var testpic_index=0;
 
-
+//returns a new image-url with current parameters
     var imgurl = function(skip_buffered_images){
+//verify the image for a camera picture
         if(image_query.statusText == "Ok" ) {
             j=j+1;
             var img_url = "/image?gain="+Math.floor(gain)+
@@ -33,6 +40,7 @@ $(function(){
                 "&skip_buffered_images="+skip_buffered_images;
             return img_url;
         }
+//if no picture is given it uses the testpics which was create before
         else{
             testpic_index++;
             var newpic=testpics[testpic_index%7];
@@ -40,11 +48,11 @@ $(function(){
         }
     };
 
-
+// changes the attribute of the live image-url
     var update_image = function() {
         $('#liveImage').attr("src",imgurl(true))
     };
-
+//sets interval between each picture which is given from the server
     var timerid;
     var live_update = function() {
         var img = $('#liveImage');
@@ -55,18 +63,18 @@ $(function(){
             },100);
         });       
     };
-
+//cancel timer, no image from the server anymore, no new picture is shown at the browser
     var cancel_live_update = function() {
         clearTimeout(timerid);
         $('#liveImage').off('load');
     };
-
+//changes the format attribute at the image-url, format at the shown picture changes 
     $("#format").change(function(){
         format = $(this).val();
         console.log(format);
         update_image();
     });
-
+//resets the attribute to default again
     $("#reset").click(function(){
         gain = 0;
         shutter = 50;
@@ -74,7 +82,7 @@ $(function(){
         contrast = 50;
         saturation = 50;
     });
-
+//changes the gain attribute at the image-url, gain at the shown picture changes
     $("#gainslider").noUiSlider("init",{
         dontActivate: "lower", startMax: gain,
         change: function() {
@@ -83,6 +91,7 @@ $(function(){
             update_image();
         }
     });
+//changes the shutter attribute at the image-url, shutter at the shown picture changes
     $("#shutterslider").noUiSlider("init",{
         dontActivate: "lower", startMax: shutter,
         change: function() {
@@ -91,6 +100,7 @@ $(function(){
             update_image();
         }
     });
+//changes the brightness attribute at the image-url, brightness at the shown picture changes
     $("#brightnessslider").noUiSlider("init",{
         dontActivate: "lower", startMax: brightness,
         change: function() {
@@ -99,6 +109,7 @@ $(function(){
             update_image();
         }
     });
+//changes the contrast attribute at the image-url, contrast at the shown picture changes
     $("#contrastslider").noUiSlider("init",{
         dontActivate: "lower", startMax: contrast,
         change: function() {
@@ -107,6 +118,7 @@ $(function(){
             update_image();
         }
     });
+//changes the saturation attribute at the image-url, saturation at the shown picture changes
     $("#saturationslider").noUiSlider("init",{
         dontActivate: "lower", startMax: saturation,
         change: function() {
@@ -117,13 +129,13 @@ $(function(){
     });
 
 
-
+//saves the picture at the next image spot, the last picture at the last spot deleted
     $("#single").click(function(){
         var new_image = $('<img class="span2">').attr('src',imgurl(true));
         $('#imageHistory').prepend(new_image);
         $('#imageHistory').children(':last-child').remove();
     });
-
+//changes the live-button, triggers the sets interval again
     var clicked = false;
     $('#Live').click(function(){
         clicked = !clicked;
@@ -133,5 +145,6 @@ $(function(){
             cancel_live_update();
         }
     });
+//explicitly trigger the click event of the life button to a life image per default, button clicked when website opens
     $('#Live').trigger('click');
 });
